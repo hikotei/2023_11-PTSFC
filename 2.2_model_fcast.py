3368365ecd5d7e6550f430cd8ff6c3b87072d17a
Yanting <ytl_cc@hotmail.de>
2023-12-07 08:06:10 +0100

    train n eval

--- 2.2_model_fcast.py
+++ 2.2_model_fcast.py
@@ -1,74 +1,138 @@
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# import libraries
 
-# train test split
+import os
+import numpy as np
+import pandas as pd
+import pmdarima as pm
+
+from datetime import datetime
+from itertools import product
+
+import platform
+if 'win' in platform.platform():
+    os.environ['R_HOME'] = 'C:\Program Files\R\R-4.3.2'
+
+import rpy2
+import rpy2.robjects.packages as rpackages
+import rpy2.robjects as robjects
+
+import rpy2.robjects.numpy2ri
+rpy2.robjects.numpy2ri.activate()
+
+rugarch = rpackages.importr('rugarch')
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# import data and do train test split
 
-# fit ARIMA models using pmdarima to find best order
+today = datetime.today().strftime("%Y-%m-%d")
+fname = f"2000-01-01_{today}_dax_returns.csv"
+
+# Load data
+ret_df = pd.read_csv(f"./data/{fname}", index_col=0)
+ret_df.index = pd.to_datetime(ret_df.index)
+
+ignore_first = 2000
+ret_df = ret_df.iloc[ignore_first:]
+
+# train test split
+split_pt = 0.8
+train_data = ret_df[:int(len(ret_df) * split_pt)]
+test_data = ret_df[int(len(ret_df) * split_pt):]
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# params for model fitting
+
+max_p_q = 5
+
+# Define the parameter space
+p_values = range(1,max_p_q)  # AR order
+q_values = range(1,max_p_q)  # MA order
+r_values = [1, 2]  # GARCH order
+s_values = [1, 2]  # ARCH order
+
+arma_garch_search_params = product(p_values, q_values, r_values, s_values)
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# actual model fitting
+
+# - - - - - - - - - - - - - -
+arma_models = {}
+
+for i, ret_horizon in enumerate(train_data.columns):
+
+    # fit ARIMA models using pmdarima to find best order
+    arma_models[horizon] = pm.auto_arima(train_data[horizon], start_p=1, start_q=1,
+                                         max_p=max_p_q, max_q=max_p_q, trace=True,
+                                         error_action='ignore',
+                                         suppress_warnings=True,
+                                         stepwise=True)
+
+# - - - - - - - - - - - - - -
+arma_garch_models = {}
 
 # fit ARIMA + GARCH models using rpy2
 
-    # implement own grid search for best order
-    # also include best order from ARIMA model
+for i, ret_horizon in enumerate(train_data.columns):
 
-# save all models in dict: model params + in sample fcasts
+    for i, params in enumerate(arma_garch_search_params):
 
-# - - - - - - - 
+        mean_model = robjects.ListVector(
+            {'armaOrder': robjects.IntVector([p, q])})
 
-# maybe try other variants of GARCH models :
+        variance_model = robjects.ListVector(
+            {'model': "sGARCH",
+            'garchOrder': robjects.IntVector([arch_p, arch_q])})
 
-    # EGARCH, 
-    # GJR-GARCH, 
-    # GARCH-M, 
-    # TGARCH, AVGARCH, NGARCH, etc. 
-    # and/or stochastic volatility models
+        if dist == 'std':
+            model = rugarch.ugarchspec(variance_model=variance_model, mean_model=mean_model, distribution_model=dist, fixed_pars=r_std_params)
 
-# - - - - - - - 
-
-# Note that there does not seem to be an option to use SARMA models in the "rugarch" package. 
-# But if there is a seasonal pattern (and that is quite likely when it comes to tourist arrivals), you will have to account for it somehow. 
-# Consider using exogenous seasonal variables (dummies or Fourier terms) in the conditional mean model 
-# via the argument external.regressors inside the argument mean.model in function ugarchspec. 
-# Alternatively, note that a SARMA model corresponds to a restricted ARMA model. 
-# An approximation of SARMA could thus be an ARMA with the appropriate lag order but without the SARMA-specific parameter restrictions.
-# (see https://stats.stackexchange.com/a/176629/338210)
-
-# - - - - - - - 
-
-# Q = Is a GARCH model even necessary?
-# A = test residuals from the SARIMA model using ARCH-LM test or some other test for (G)ARCH effects.
-
-    # If we discover conditional heteroskedasticity in the residuals of an ARMA model, 
-    # it certainly makes sense to try appending a GARCH specification to ARMA to see what happens.
-
-# ARMA alone would explain more variance in sample than ARMA-GARCH 
-# (just as OLS would explain more than feasible GLS, regardless of which is closer to the true model in population). 
-# GARCH would not explain any variance if you leave the conditional mean part empty (without ARMA). 
-# And if the ARMA-GARCH model approximates the true DGP better than a plain ARMA and plain GARCH, 
-# the out of sample performance of ARMA-GARCH will be better -- as long as you can estimate the model sufficiently well. 
-# (And since ARMA-GARCH is a richer model than plain ARMA and plain GARCH, you would normally not be able to estimate it as precisely as plain ARMA and plain GARCH on any given dataset.)
-# (see https://stats.stackexchange.com/a/233165/338210)
+        elif dist == "norm":
+            model = rugarch.ugarchspec(variance_model=variance_model, mean_model=mean_model, distribution_model=dist)
+        
+        model_name = f"{ret_horizon}_({p},{q})_({arch_p}, {arch_q})"
+        arima_garch_model[model_name] = rugarch.ugarchfit(spec=model, data=train_data, solver='hybrid', tol=1e-3) 
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
-
-# evaluate models on test set using rolling window fcast
-
-# in theory : 
-
-    # should select model with out of sample criteria
-    # under the assumptions of standard ARMA-GARCH, the residuals should be homoscedastic and more generally iid normal => TEST
-
-    # AIC estimates the model's likelihood of a new observation coming from the same distribution. 
-    # It does so based on in-sample information, i.e. uses in-sample likelihood and penalizes it for overfitting. 
-    # To underline, AIC does not measure in-sample fit, it estimates out-of-sample fit (expected likelihood of a new observation).
-    # (see https://stats.stackexchange.com/questions/159807/arma-garch-model-selection-fit-evaluation?rq=1#:~:text=AIC%20estimates%20the%20model%27s%20likelihood%20of%20a%20new%20observation%20coming%20from%20the%20same%20distribution)
+# evaluate models on remaining test set using rolling window fcast
 
 # create results df with:
 
-    # model params ie p, q, arch_p, arch_q
-    # model information criteria
-    # model log likelihood
+    #     model params ie p, q, arch_p, arch_q
+    #     model information criteria
+    #     model log likelihood
 
-    # in sample errors: MSE, MAE, MAPE, SMAPE
-    # out of sample fcast errors: MSE, MAE, MAPE, SMAPE
+    #     in sample errors: MSE, MAE, MAPE, SMAPE
+    #     out of sample fcast errors: MSE, MAE, MAPE, SMAPE
+
+results_df = pd.DataFrame(results)
+
+for model_name, model in arma_models.items():
+
+    fitted_values = model.fittedvalues()
+    error = model.resid()
+
+    # rolling window forecast
+
+    for i in range(len(test_data)):
+
+        # fit model on training data
+        model = ARIMA(train_data[:i], order=model['order'])
+        model_fit = model.fit()
+
+        # forecast one step ahead
+        y_hat = model_fit.forecast()[0]
+
+        # store forecast and ob
+        forecasts.append(yhat)
+        obs.append(test_data[i])
+
+    results.append({
+        'model': model_name,
+        'order': model['order'],
+        'in_sample_fcast': model['in_sample_fcast'],
+        'errors': errors
+    })
+
+for model_name, model in arima_garch_model.items():
