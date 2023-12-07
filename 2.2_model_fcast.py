b7625f4b34aba8185d2ec3845e0a080bbec4d1b6
Yanting <ytl_cc@hotmail.de>
2023-12-07 05:50:48 +0100

    arma garch

---
+++ 2.2_model_fcast.py
@@ -0,0 +1,16 @@
+
+# train test split
+
+# fit ARIMA models
+# fit ARIMA + GARCH models
+
+# save models in dict: model params + in sample fcasts
+# evaluate models on test set using rolling window fcast
+
+# create results df with:
+
+    # model params ie p, q, arch_p, arch_q
+    # model information criteria
+    # model log likelihood
+    # in sample errors: MSE, MAE, MAPE, SMAPE
+    # out of sample fcast errors: MSE, MAE, MAPE, SMAPE
